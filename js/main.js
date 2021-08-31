3317c85a313e15cb96432af65f58e0b48455bcfc
Elisa Nicot <nico0122@algonquinlive.com>
2021-08-31 08:59:41 -0400

    Update main.js page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var newName = prompt('What is your name?');
-if (newName == 'Elisa'){
+
+if (newName == 'Elisa') {
   alert('Hey Elisa, our names are the same');
 } else {
   alert('Too bad our names are different, Thomas.');
